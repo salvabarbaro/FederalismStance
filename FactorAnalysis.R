5a9e83f5bb66bc0731ec07b61d2ce16c519dc8c2
Salvatore Barbaro <58938667+salvabarbaro@users.noreply.github.com>
2024-11-13 16:54:53 +0100

    Update FactorAnalysis.R

--- FactorAnalysis.R
+++ FactorAnalysis.R
@@ -32,25 +32,26 @@ plot.data <- left_join(x = bund_shp, y = df.short, by = "abbreviation")  # for g
 #1. Effect of government-heading parties
 kw1 <- df %>% kruskal_test(fedatt.micro~party)
 mw1 <- df %>% wilcox_test(fedatt.micro~party, alternative = "greater")
-#2. Effect of Party shares
-party.lm <- lm(data = df, fedatt.micro ~ rCDU_CSU + rFDP + rSPD + rGrüne + rLinke)
-summary(party.lm)
-#texreg::texreg(party.lm)
+#2.1 Effect of Party shares (POLR)  [Supplementary Material]
+library(MASS)
+party.polr <- polr(data = df, 
+                   as.factor(fedatt.micro) ~ rCDU_CSU + rFDP + rSPD + rGrüne + rLinke, 
+                   Hess = T)
 ###################################################################################################################
 ## 3.  Prussian tradition
 kw2 <- df %>%  kruskal_test(fedatt.micro~Prussian.char)
 dt2<- df %>%  dunn_test(fedatt.micro~Prussian.char)
 #
 #####################################################################################################################
-## 4. Catholic Share
+## 4. Catholic Share  [descriptive]
 ## see plotscript
 ##########################################################################
-## 5. FKM
+## 5. FKM [descriptive]
 ## see plotscript
 ##6. Average attitude values (plot on map)
 ## see plotscript
 #############################################################################
-#### OLS
+#### OLS   [Supplementary Material]
 df <- df %>% mutate(Prussian.na  = ifelse(Prussian.char == "no", 0, ifelse(Prussian.char == "mixed", NA, 1)),
                     .after = Prussian.char)
 mod1 <- lm(formula = fedatt.micro ~ cath.share21 + FKM21 + Prussian.char +
@@ -65,16 +66,8 @@ mod3 <- lm(formula = fedatt.micro ~ cath.share21 + FKM21 + Prussian.na +
 mod4 <- lm(formula = fedatt.micro ~ cath.share21 + FKM21 + 
              rCDU_CSU + rFDP + rSPD + rGrüne + rLinke,
            data = df)
-#texreg::texreg(l = list(mod1, mod2, mod3, mod4),
-#               custom.model.name =c("(S.1)", "(2)", "(3)", "(4)"),
-#               caption="OLS regressions: base model and model variations",
-#               caption.above=TRUE,
-#               single.row=TRUE,
-#               digits=3,
-#               file = "basemodels.tex"
-#               )
-
-library(plm)
+#
+library(plm)  [Appendix (fixed effects)]
 #
 mod_S1plm <- plm(fedatt.micro ~ cath.share21 + FKM21 + rCDU_CSU + rFDP + rSPD + rGrüne + rLinke,
                  index = c("Prussian.char"), model = "within", data = df)
@@ -82,22 +75,12 @@ mod_S2plm <- plm(fedatt.micro ~ cath.share21 + FKM21 + rCDU_CSU + rFDP + rSPD + 
                  index = c("Prussian.na"), model = "within", data = df %>% filter(., is.na(Prussian.na) == F))
 mod_S3plm <- plm(fedatt.micro ~ cath.share21 + FKM21 + Prussian.char + rCDU_CSU + rFDP + rSPD + rGrüne + rLinke,
                  index = c("party"), model = "within", data = df %>% filter(., is.na(party) == F))
-# 
-#texreg(l = list(mod_S1plm, mod_S2plm, mod_S3plm),
-#       custom.model.name = c("S.1", "S.2", "S.3"),
-#       caption = "OLS regression with fixed effects",
-#       caption.above = TRUE,
-#       single.row = TRUE,
-#       digits = 3,
-#       file = "~/Documents/Research/Yardstick/BR01/modelspecs.tex"
-#       )
-#
 #summary(mod_S3plm)
 ######################################################################################
 # Ordinal logistic regression (we use for higher stability the MASS package, with fixed effect we turn to the ordinal package)
 ## Main models
 library(ordinal)  # For ordinal log reg with fixed / random effects
-library(MASS)     # For ordinal log reg (polr)
+library(MASS)     # For ordinal log reg (polr), also FE.
 library(brant)    # Brant test (proportional odds assumption)
 #library(gtsummary)  # for display summary tables
 mod01 <- polr(formula = as.factor(fedatt.micro) ~ cath.share21 + 
@@ -123,35 +106,6 @@ mod04 <- polr(formula = as.factor(fedatt.micro) ~ cath.share21 + FKM21 +
 #tbl_regression(mod04, exponentiate = T)
 brant(mod04)
 #broom::tidy(mod04, exponentiate = T, conf.int = T)
-
-## Export results to a LaTeX table:
-#polr.list <- list(mod01, mod02, mod03, mod04)
-#coef_namesShort <- list(
-#  "cath.share21" = "Catholic Share 2021",
-#  "FKM21" = "Financial Strength",
-#  "Prussian.charno" = "Prussian:no",
-#  "Prussian.charyes" = "Prussian:yes",
-#  "rCDU_CSU" = "Election Results (UNION)",
-#  "rSPD" = "Election Results (SPD)",
-#  "rFDP" = "Election Results (FDP)",
-#  "rGrüne" = "Election Results (GREEN)"
-#)
-#texreg::texreg(l = polr.list,
-#               override.coef = lapply(polr.list, function(model) exp(extract(model)@coef)),
-#               override.ci.low = lapply(polr.list, function(model) exp(extract(model)@coef - 1.96 * extract(model)@se)),
-#               override.ci.up = lapply(polr.list, function(model) exp(extract(model)@coef + 1.96 * extract(model)@se)),
-#               ci.test = 1,
-#               custom.coef.map = coef_namesShort,
-#               omit.coef = "1|2",
-#               custom.model.name =c("(1)", "(2)", "(3)", "(4)"),
-#               caption="Ordinal logistic regressions: base model and model variations",
-#               caption.above=TRUE,
-#               single.row=F,
-#               digits=3,
-#               booktabs = TRUE, use.packages = F,
-#               label = "tb.polr",
-#               file = "polr01.tex"
-#)
 #############   Robustness Checks -- Fixed Effects (Table 6)
 mod_S1clm <- clm(as.factor(fedatt.micro) ~ cath.share21 + FKM21 + 
                    rCDU_CSU + rFDP + rSPD + rGrüne  +
@@ -174,22 +128,6 @@ summary(mod_S3clm)
 #
 clmFE.list <- list(mod_S1clm, mod_S2clm, mod_S3clm)
 #
-#texreg::texreg(l = clmFE.list,
-#               override.coef = lapply(clmFE.list, function(model) exp(extract(model)@coef)),
-#               override.ci.low = lapply(clmFE.list, function(model) exp(extract(model)@coef - 1.96 * extract(model)@se)),
-#               override.ci.up = lapply(clmFE.list, function(model) exp(extract(model)@coef + 1.96 * extract(model)@se)),
-#               ci.test = 1,
-#               custom.coef.map = coef_namesShort,
-#               custom.model.name =c("(S.1)", "(S.2)", "(S.3)"),
-#               caption="Ordinal logistic regressions with fixed effects",
-#               caption.above=TRUE,
-#               single.row=F,
-#               digits=3,
-#               booktabs = TRUE, use.packages = F,
-#               label = "tb.clmFE",
-#               file = "~/Documents/Research/Yardstick/BR01/Publius2024RR/vNOV24/clmFE01.tex"
-#)
-
 ## East-West as FE
 westld <- c("BW", "BY", "HE", "RP", "SL", "NI", "NW", "SH", "HH", "HB")
 eastld <- c("TH", "SN", "ST", "BR", "MV")
@@ -207,23 +145,6 @@ mod_EW1polr <- polr(as.factor(fedatt.micro) ~ cath.share21 + FKM21 +
                       factor(eastwest),
                     data = df.eastwest, Hess = TRUE)
 #
-#EastWest.list <- list(mod_EW1polr)
-#texreg::texreg(l = EastWest.list,
-#               override.coef = lapply(EastWest.list, function(model) exp(extract(model)@coef)),
-#               override.ci.low = lapply(EastWest.list, function(model) exp(extract(model)@coef - 1.96 * extract(model)@se)),
-#               override.ci.up = lapply(EastWest.list, function(model) exp(extract(model)@coef + 1.96 * extract(model)@se)),
-#               ci.test = 1,
-#               custom.coef.map = coef_namesShort,
-#               custom.model.name =c("Main Model"),
-#               caption="Ordinal logistic regressions with East vs. West as fixed effects",
-#               caption.above=TRUE,
-#               single.row=T,
-#               digits=3,
-#               booktabs = TRUE, use.packages = F,
-#               label = "tb.eastwest",
-#               file = "~/Documents/Research/Yardstick/BR01/Publius2024RR/vNOV24/clmEastWest.tex"
-#)
-
 ########################################
 ## BOOTSTRAP (Monte Carlo) Method for estimator accuracy.
 ## (Supplementary Material)
@@ -243,12 +164,12 @@ bootstrap_results <- boot(data = df, statistic = run_lm_on_bootstrap, R = num_bo
 coeff.bootstrap <- as.data.frame(bootstrap_results$t) %>% 
   setNames(do.call(cbind, list(names(coefficients(mod1)))))
 
-ggplot(data = coeff.bootstrap, 
-       aes(y = FKM21)) +
-  geom_histogram(fill = "forestgreen", alpha = 0.4) +
-  geom_hline(yintercept = coefficients(mod1)[3], col = "purple") +
-  labs(y = "Financial Strength Estimator")
-ggsave("bootstrapFKM.pdf")
+#ggplot(data = coeff.bootstrap, 
+#       aes(y = FKM21)) +
+#  geom_histogram(fill = "forestgreen", alpha = 0.4) +
+#  geom_hline(yintercept = coefficients(mod1)[3], col = "purple") +
+#  labs(y = "Financial Strength Estimator")
+#ggsave("bootstrapFKM.pdf")
 
 ### Regional identity  (Supplementary Material)
 identity.df <-  data.frame(abbreviation = unique(df$abbreviation), 
